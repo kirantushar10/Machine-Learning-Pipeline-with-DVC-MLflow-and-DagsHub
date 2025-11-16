373412d9512d5aa454241fcae2639e09ebe54d66
Tushar Kiran <kirantushar10@gmail.com>
2025-11-16 19:09:17 +0100

    FINAL CHANGES

---
+++ src/evaluate.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import pickle
+import mlflow
+import yaml
+from sklearn.metrics import accuracy_score
+import os
+
+os.environ['MLFLOW_TRACKING_URI'] = "https://dagshub.com/kirantushar10/MachineLearningPipeline.mlflow"
+os.environ['MLFLOW_TRACKING_USERNAME'] = "kirantushar10"
+os.environ['MLFLOW_TRACKING_PASSWORD'] = "a6a098b1c426bb934bc0dda1759164bb1136d7b0"
+
+## LOAD PARAMETERS FROM YAML
+
+params = yaml.safe_load(open("params.yaml"))["train"]
+
+def evaluate(data_path,model_path):
+    data = pd.read_csv(data_path)
+
+    X=data.drop(columns=["Outcome"])
+    y=data["Outcome"]
+
+    mlflow.set_tracking_uri("https://dagshub.com/kirantushar10/MachineLearningPipeline.mlflow")
+
+    ## LOAD THE MODEL FROM THE DISK
+
+    model = pickle.load(open(model_path,'rb'))
+
+    predictions = model.predict(X)
+    accuracy = accuracy_score(y,predictions)
+
+    ## LOG THE METRICS
+    mlflow.log_metric("Accuracy",accuracy)
+    print(f"Model Accuracy:{accuracy}")
+
+if __name__=="__main__":
+    evaluate(params["data"],params['model'])
+
+
---
+++ src/train.py
@@ -0,0 +1,100 @@
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+import pickle
+import mlflow
+import yaml
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from mlflow.models import infer_signature
+import os
+from sklearn.model_selection import train_test_split, GridSearchCV
+from urllib.parse import urlparse
+
+os.environ['MLFLOW_TRACKING_URI'] = "https://dagshub.com/kirantushar10/MachineLearningPipeline.mlflow"
+os.environ['MLFLOW_TRACKING_USERNAME'] = "kirantushar10"
+os.environ['MLFLOW_TRACKING_PASSWORD'] = "a6a098b1c426bb934bc0dda1759164bb1136d7b0"
+
+def hyperparameter_tuning(X_train,y_train,param_grid):
+    rf = RandomForestClassifier()
+    grid_search = GridSearchCV(estimator=rf, param_grid=param_grid, cv=3,n_jobs=1,verbose=2)
+    grid_search.fit(X_train,y_train)
+    return grid_search
+
+## LOAD PARAMETERS FROM YAML
+
+params = yaml.safe_load(open("params.yaml"))["train"]
+
+def train(data_path,model_path,random_state,n_estimators,max_depth):
+    data = pd.read_csv(data_path)
+    X=data.drop(columns=["Outcome"])
+    y=data["Outcome"]
+
+    mlflow.set_tracking_uri("https://dagshub.com/kirantushar10/MachineLearningPipeline.mlflow")
+
+    ## START THE MLFLOW RUN
+
+    with mlflow.start_run():
+
+        ## SPLIT THE DATASET INTO TRAINING AND TEST SETS
+        X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.20,random_state=random_state)
+        signature = infer_signature(X_train,y_train)
+
+        ## DEFINE HYPER PARAMETERS GRID
+
+        params_grid = {
+            'n_estimators': [100,200],
+            'max_depth' : [5,10,None],
+            'min_samples_split': [2,5],
+            'min_samples_leaf' : [1,2]
+        }
+        ## PERFORM HYPER PARAMETERS TUNING 
+        grid_search = hyperparameter_tuning(X_train,y_train,params_grid)
+
+        ## GET THE BEST MODEL
+        best_model = grid_search.best_estimator_
+
+        ## PREDICT AND EVALUATE THE MODEL
+
+        y_pred = best_model.predict(X_test)
+        accuracy= accuracy_score(y_test,y_pred)
+        print(f"Accuracy:{accuracy}")
+
+        ## LOG ADDITIONAL METRICS
+        mlflow.log_metric("Accuracy",accuracy)
+        mlflow.log_param("best_n_estimators",grid_search.best_params_['n_estimators'])
+        mlflow.log_param("best_max_depth",grid_search.best_params_['max_depth'])
+        mlflow.log_param("best_min_samples_split",grid_search.best_params_['min_samples_split'])
+        mlflow.log_param("best_min_samples_leaf",grid_search.best_params_['min_samples_leaf'])
+
+        ## LOG THE CONFUSION MATRIX
+
+        cm = confusion_matrix(y_test,y_pred)
+        cr = classification_report(y_test,y_pred)
+
+        mlflow.log_text(str(cm),"confusion_matrix.txt")
+        mlflow.log_text(str(cr),"classification_report.txt")
+
+        # tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+        # if tracking_url_type_store != 'file':
+            # mlflow.sklearn.log_model(best_model,"model",registered_model_name="Best Model")
+        # else:
+            # mlflow.sklearn.log_model(best_model,"model",signature=signature)
+
+        # mlflow.sklearn.log_model(best_model, "best_model", signature=signature)
+        
+        ## CREATE A DIR TO SAVE THE MODEL
+        os.makedirs(os.path.dirname(model_path),exist_ok=True)
+
+
+        filename = model_path
+        pickle.dump(best_model,open(filename,'wb'))
+
+        # Log as MLflow artifact instead of MLflow model
+        mlflow.log_artifact(filename, artifact_path="model")
+
+        print(f"Model saved to {model_path}")
+
+
+if __name__ == "__main__":
+    train(params['data'],params['model'],params['random_state'],params['n_estimators'],params['max_depth'])
